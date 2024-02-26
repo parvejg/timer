02f32e415c98c6f0b9841c8ca9f1dc1471e37502
parvejg <parvezpg321@gmail.com>
2024-02-27 01:50:02 +0530

    complete structure work

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,10 +1,15 @@
+import "./Timer.css"
 export const Timer = ()=>{
     return(
         <div className="timer-container">
             <div className="timer">
-                <input placeholder="Enter here" type="number" min="0" max="59" />
+                <input className="timer-input" placeholder="Enter here" type="number" min="0" max="59" />
                 <div className="output-box">
-                    <h1>08m 45s</h1>
+                    <h1 className="timer-time">08m 45s</h1>
+                </div>
+                <div className="timer-btn-wrapper">
+                    <button className="timer-btn">Start</button>
+                    <button className="timer-btn">Reset</button>
                 </div>
             </div>
         </div>
